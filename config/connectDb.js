ba31a861c2dcb0c12852e551b0cbfc981680bcf4
SahTitus <sahtitus58@gmail.com>
2023-03-30 12:58:30 +0000

    mongodb fixes

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
+    mongoose.set('strictQuery', true)
     await mongoose.connect(process.env.DATABASE_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
